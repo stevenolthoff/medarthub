b3588baac6148dd3a793e6a7bc6c99a7e2b06dfe
Steven Olthoff <stef@magicwords.dev>
2025-10-02 16:57:19 -0400

    add tailwind

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5174,
     proxy: {
       '/api': 'http://localhost:3001'
+    },
+    hmr: {
+      overlay: true
     }
   },
   build: {
     outDir: 'dist'
+  },
+  css: {
+    devSourcemap: true
   }
 })
