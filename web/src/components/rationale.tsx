d78dbe0e4e9486d31c5da8c18a88c211fc34f201
Steven Olthoff <stef@magicwords.dev>
2025-11-02 18:21:10 -0500

    Extract landing page header into separate LandingHeader component

---
+++ web/src/components/rationale.tsx
@@ -0,0 +1,23 @@
+export function Rationale() {
+  return (
+    <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-3xl">
+        <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+          But why?
+        </h2>
+        <div className="mt-10 space-y-6 text-base leading-8 text-gray-600 sm:text-lg">
+          <p>
+            Early medical illustrators made careers off drawing for textbooks. It is now 2025. There are only so many textbooks to draw but hundreds of new medical illustrators graduate each year.
+          </p>
+          <p>
+            But there is still plenty of work! US biotech investment alone has grown from X in YEAR to Y in YEAR. Countless companies need to show their mechanisms of action, devices, and more. They need you. The problem? They can't find you.
+          </p>
+          <p>
+            Why can't they find you? You likely have Behance, Instagram, and your own site on Squarespace. But if a biotech marketer Googles "3D device animator" you'll never show. Each artist is an island. All business funnels to the same 10 big players.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
